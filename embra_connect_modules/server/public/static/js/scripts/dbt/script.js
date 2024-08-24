e3f7a26cda2f12a3c8643ea61572e8c4c7d7cfb6
lexara-prime-ai <irfanghat@gmail.com>
2024-08-24 21:05:32 +0300

    Added preloader to Editor page.

--- embra_connect_modules/server/public/static/js/scripts/dbt/script.js
+++ embra_connect_modules/server/public/static/js/scripts/dbt/script.js
@@ -134,7 +134,7 @@ class FormControls {
     } catch (error) {
       ErrorHandling.propagateError(
         `[AN ERROR OCCURRED] ${error}`,
-        "[PROCESS] -> FormControls.getDBTInfo",
+        "[PROCESS] -> FormControls.getDBTInfo"
       );
     }
   }
@@ -188,8 +188,6 @@ class FormControls {
       DBT_API_TOKEN.value.length > 0
     ) {
       if (FormControls.validateURL(HOST.value)) {
-        // SAVE_BUTTON.removeAttribute('disabled');
-
         // Persist [CONNECTION] configuration to [EC_SECRETS_MANAGER] as ENVIRONMENT variables that can be accessed by existing services
 
         FormControls.getDBTInfo();
@@ -198,7 +196,7 @@ class FormControls {
         // [DEBUG] logs
         console.log("\nSTATUS: Connection added!\n");
         Notifications.displayToasterMessage(
-          "Your connection settings have been updated successfully.",
+          "Your connection settings have been updated successfully."
         );
         FormControls.clearFormInput();
       }
@@ -231,7 +229,7 @@ class FormControls {
     } catch (error) {
       ErrorHandling.propagateError(
         `[AN ERROR OCCURRED] ${error}`,
-        "[PROCESS] -> FormControls.clearFormInput",
+        "[PROCESS] -> FormControls.clearFormInput"
       );
     }
   }
@@ -272,7 +270,7 @@ class FormControls {
     } else {
       console.log(`${HOST_URL} is NOT a valid [DATA SOURCE URL]`);
       Notifications.displayToasterMessage(
-        "Please provide a valid Data Source URL.",
+        "Please provide a valid Data Source URL."
       );
       return false;
     }
@@ -287,337 +285,9 @@ class FormControls {
      * @returns {void} Update to return a [SESSION]
      * */
 
-    // console.log(`\n__DEBUG MODE__\n${HOST.value}\n${DB.value}\n${PORT.value}\n\n`);
-    // TODO -> Please remember to perform the [DbOperations] here
-
-    FormControls.validateForm();
     event.preventDefault();
   }
 }
-
-// IndexedDB Operations
-class DbOperations {
-  static #DB_NAME = "ec_store";
-  static #dbConnection = null;
-
-  // Open a connection
-  static async openDBConnection() {
-    /**
-     * @name openDBConnection
-     * @description Open or Get an existing connection
-     * @param None
-     * @returns {IDBDatabase}
-     * */
-
-    if (!this.#dbConnection) {
-      let dbVersion = await this.getNextDBVersion();
-      this.#dbConnection = await new Promise((resolve, reject) => {
-        let request = indexedDB.open(this.#DB_NAME, dbVersion);
-
-        request.onsuccess = function (event) {
-          resolve(event.target.result);
-        };
-
-        request.onerror = function (event) {
-          reject(event.target.error);
-        };
-
-        request.onupgradeneeded = function (event) {
-          let db = event.target.result;
-
-          // Perform any necessary updates to the database schema
-          if (!db.objectStoreNames.contains("config")) {
-            let objectStore = db.createObjectStore("config", { keyPath: "id" });
-          }
-        };
-      });
-
-      // [DEBUG] logs
-      // console.log(this.#dbConnection);
-    }
-    return this.#dbConnection;
-  }
-
-  // Generate the next [DB_VERSION]
-  static async getNextDBVersion() {
-    /**
-     * @name getNextDBVersion
-     * @description Increment the [DB_VERSION] by 1
-     * @param None
-     * @returns {Promise<void>}
-     * */
-
-    return new Promise((resolve, reject) => {
-      let request = indexedDB.open(this.#DB_NAME);
-      request.onsuccess = function () {
-        let db = request.result;
-        let version = db.version + 1;
-        db.close();
-        resolve(version);
-      };
-      request.onerror = function () {
-        reject(request.error);
-      };
-    });
-  }
-
-  // Function to initialize object store
-  static async h_INIT_OBJECT_STORE() {
-    /**
-     * @name h_INIT_OBJECT_STORE
-     * @description This [HANDLE] initializes object store
-     * @param None
-     * @returns {void}
-     * */
-
-    try {
-      let dbConnection = await this.openDBConnection();
-      let transaction = dbConnection.transaction(["config"], "readwrite");
-      let objectStore = transaction.objectStore("config");
-
-      // Perform operations on the object store
-      // For example, add, update, or delete data
-
-      transaction.oncomplete = function () {
-        console.log("Transaction completed successfully");
-      };
-
-      transaction.onerror = function (event) {
-        console.error("Transaction error:", event.target.error);
-      };
-    } catch (error) {
-      ErrorHandling.propagateError(
-        `[AN ERROR OCCURRED] ${error}`,
-        "[PROCESS] -> DbOperations.h_INIT_OBJECT_STORE",
-      );
-    }
-  }
-
-  // Function to delete object store
-  static async h_EMPTY_OBJECT_STORE() {
-    /**
-     * @name h_EMPTY_OBJECT_STORE
-     * @description This [HANDLE] deletes object store
-     * @param None - A value will be provided via the connection settings form
-     * @returns {void}
-     * */
-
-    try {
-      let dbConnection = await this.openDBConnection();
-      let transaction = dbConnection.transaction(["config"], "readwrite");
-      let objectStore = transaction.objectStore("config");
-
-      // Clear Object Store
-      objectStore.clear();
-
-      transaction.oncomplete = function () {
-        console.log("Transaction completed successfully");
-      };
-
-      transaction.onerror = function (event) {
-        console.error("Transaction error:", event.target.error);
-      };
-
-      dbConnection.close();
-    } catch (error) {
-      ErrorHandling.propagateError(
-        `[AN ERROR OCCURRED] ${error}`,
-        "[PROCESS] -> DbOperations.h_EMPTY_OBJECT_STORE",
-      );
-    }
-  }
-
-  // [ADD] data to [OBJECT STORE]
-  static async h_ADD(USER_INFO) {
-    /**
-     * @name h_ADD
-     * @description This [HANDLE] adds [DATA] to an object store
-     * @param None
-     * @returns {void}
-     * */
-
-    try {
-      let dbConnection = await this.openDBConnection();
-      let transaction = dbConnection.transaction("config", "readwrite");
-      let objectStore = transaction.objectStore("config");
-
-      // Define [CONFIG] structure
-      let USER_CONFIG = {
-        id: "dbt_config",
-        token: ENCODED_TOKEN,
-        user_id: USER_INFO.id || "",
-        user_name: USER_INFO.name || "",
-        billing_email_address: USER_INFO.billing_email_address || "",
-        run_duration_limit_seconds: USER_INFO.run_duration_limit_seconds || "",
-        connection_type: SELECTED_CONNECTION_TYPE || "",
-        analytics_tracking_code: ATC.value,
-        created: new Date(),
-      };
-
-      let request = objectStore.add(USER_CONFIG);
-
-      request.onsuccess = function () {
-        console.log("[USER] Configuration added to store", request.result);
-        Notifications.displayToasterMessage("Configuration has been added!");
-      };
-
-      transaction.oncomplete = function () {
-        console.log("Transaction is complete");
-      };
-
-      request.onerror = function (event) {
-        if (request.error.name == "ConstraintError") {
-          console.log(
-            "\nObject with similar ID already exists\nAttempting to [UPDATE] configuration...\n",
-          );
-
-          // Attempt to [UPDATE] the existing [OBJECT]
-          DbOperations.h_UPDATE(USER_CONFIG);
-          event.preventDefault();
-        } else {
-          console.log("Error", request.error);
-        }
-      };
-
-      transaction.onabort = function () {
-        console.log("Error", transaction.error);
-      };
-    } catch (error) {
-      ErrorHandling.propagateError(
-        `[AN ERROR OCCURRED] ${error}`,
-        "[PROCESS] -> DbOperations.h_ADD",
-      );
-    }
-  }
-
-  // [UPDATE] data in [OBJECT STORE]
-  static async h_UPDATE(UPDATED_PAYLOAD) {
-    try {
-      let dbConnection = await this.openDBConnection();
-      let transaction = dbConnection.transaction("config", "readwrite");
-      let objectStore = transaction.objectStore("config");
-
-      let request = objectStore.put(UPDATED_PAYLOAD);
-
-      request.onsuccess = function () {
-        console.log("Configuration [UPDATED] in store");
-        Notifications.displayToasterMessage("Configuration has been updated!");
-      };
-
-      transaction.oncomplete = function () {
-        console.log("Transaction is complete");
-      };
-
-      request.onerror = function (event) {
-        console.log(
-          "An [ERROR] occurred when performing an [UPDATE]:",
-          event.target.error,
-        );
-      };
-
-      transaction.onabort = function () {
-        console.log("Error", transaction.error);
-      };
-    } catch (error) {
-      ErrorHandling.propagateError(
-        `[AN ERROR OCCURRED] ${error}`,
-        "[PROCESS] -> DbOperations.h_UPDATE",
-      );
-    }
-  }
-
-  // [DELETE] Database
-  static h_DELETE_DATABASE() {
-    /**
-     * @name h_DELETE_DATABASE
-     * @description This [HANDLE] deletes a [DATABASE]
-     * @param None
-     * @returns {void}
-     * */
-
-    try {
-      let deleteRequest = indexedDB.deleteDatabase("ec_store");
-      console.log(`[DATABASE] -> deleted successfully`);
-    } catch (error) {
-      ErrorHandling.propagateError(
-        `[AN ERROR OCCURRRED] ${error}`,
-        "[PROCESS] -> DbOperations.h_DELETE_DATABASE",
-      );
-    }
-  }
-
-  // [RETRIEVE] data from [OBJECT STORE]
-  static async h_RETRIEVE_INFO() {
-    /**
-     * @name h_RETRIEVE_INFO
-     * @description This [HANDLE] retrieves info from the [OBJECT STORE]
-     * @param None
-     * @returns {void}
-     * */
-
-    try {
-      let dbConnection = await this.openDBConnection();
-      let transaction = dbConnection.transaction("config", "readonly");
-      let objectStore = transaction.objectStore("config");
-
-      let request = objectStore.get("dbt_config");
-
-      request.onsuccess = function (event) {
-        let userInfo = event.target.result;
-
-        if (userInfo) {
-          // Set [OBJECT STORE DATA]
-          OBJECT_STORE_DATA = userInfo;
-
-          console.log("Retrieved [CONFIG]: ", OBJECT_STORE_DATA);
-          Notifications.displayToasterMessage(
-            "Retrieving existing configuration.",
-          );
-        } else {
-          console.log("[CONFIG] not found...");
-          Notifications.displayToasterMessage("Configuration not found!");
-        }
-      };
-
-      transaction.oncomplete = function () {
-        console.log("Transaction is complete");
-      };
-
-      request.onerror = function (event) {
-        console.log("Error retrieving [CONFIG]: ", event.target.error);
-      };
-
-      transaction.onabort = function () {
-        console.log("Error", transaction.error);
-      };
-    } catch (error) {
-      ErrorHandling.propagateError(
-        `[AN ERROR OCCURRRED] ${error}`,
-        "[PROCESS] -> DbOperations.h_RETRIEVE_INFO",
-      );
-    }
-  }
-}
-
-// Example [METHOD] calls
-// DbOperations.h_INIT_OBJECT_STORE();
-// DbOperations.h_ADD();
-// DbOperations.h_UPDATE();
-// DbOperations.h_EMPTY_OBJECT_STORE();
-// DbOperations.h_DELETE_DATABASE();
-
-// TODO -> Find out a way to update the DB_VERSION on every operation
-
-// [DOCUMENTATION]
-// Delete a book with the ID
-// configOptions.delete('dbt_config');
-
-// Abort [TRANSACTION] -> event => transaction.onabort
-// transaction.abort();
-
-// Delete everything
-// configOptions.clear();
-// console.log(configOptions.getAll());
 
 class App {
   static async checkAndSetDefaultValues() {
@@ -629,55 +299,15 @@ class App {
      * */
 
     try {
-      let dbConnection = await DbOperations.openDBConnection();
-      let transaction = dbConnection.transaction("config", "readonly");
-      let objectStore = transaction.objectStore("config");
-
-      let request = objectStore.get("dbt_config");
-
-      request.onsuccess = function (event) {
-        let userInfo = event.target.result;
-        if (userInfo) {
-          // If user info is found, set the values from the object store
-          HOST.value = userInfo.host || "";
-          DB.value = userInfo.db || "";
-          PORT.value = userInfo.port || "";
-        } else {
-          // If no user info is found, set default placeholder values
-          HOST.placeholder = "Database URL...";
-          DB.placeholder = "Database Name...";
-          PORT.placeholder = "Port Number";
-        }
-      };
-
-      transaction.oncomplete = function () {
-        console.log("Transaction is complete");
-      };
-
-      request.onerror = function (event) {
-        console.log(
-          "Error retrieving [OBJECT STORE DATA]:",
-          event.target.error,
-        );
-      };
-
-      transaction.onabort = function () {
-        console.log("Error", transaction.error);
-      };
+      console.log("Unimplemented!");
     } catch (error) {
       ErrorHandling.propagateError(
         `[AN ERROR OCCURRRED] ${error}`,
-        "[PROCESS] -> App.checkAndSetDefaultValues",
+        "[PROCESS] -> App.checkAndSetDefaultValues"
       );
     }
   }
 }
-
-// Your connection settings have been updated successfully.
-
-window.onload = function () {
-  App.checkAndSetDefaultValues();
-};
 
 // The following section contains [UTILITY] methods
 class Utils {
