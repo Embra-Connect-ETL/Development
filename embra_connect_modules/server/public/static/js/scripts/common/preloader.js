48dd3ea6a6cd2a2553cb218a34ed92ae7ef54bef
lexara-prime-ai <irfanghat@gmail.com>
2024-08-24 19:07:05 +0300

    Update preloader scripts.

--- embra_connect_modules/server/public/static/js/scripts/common/preloader.js
+++ embra_connect_modules/server/public/static/js/scripts/common/preloader.js
@@ -7,4 +7,27 @@ function preloader() {
 function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("app").style.display = "block";
+
+  setTimeout(() => {
+    Toastify({
+      text: "Welcome!",
+      duration: 3000,
+      newWindow: true,
+      close: false,
+      gravity: "top",
+      position: "right",
+      stopOnFocus: true,
+      style: {
+        background: "#f38041",
+        color: "#ffffff",
+        borderRadius: "9px 21px 64px 32px",
+        fontWeight: "300",
+        letterSpacing: "1.4px",
+        textTransform: "capitalize",
+        boxShadow: "0 1rem 1rem 0 rgba(0, 0, 0, .1)",
+      },
+      // Handle callback after click.
+      onClick: function () {},
+    }).showToast();
+  }, 900);
 }
